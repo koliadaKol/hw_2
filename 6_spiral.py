032591c8999bf7f17bfb09a02eae21d4d2f565c3
koliadaKol <koliadayaroslav2003@gmail.com>
2020-02-13 16:31:00 +1000

    done

--- 6_spiral.py
+++ 6_spiral.py
@@ -1,17 +1,15 @@
 import turtle
+import math
 
 turtle.speed(0)
-def spiral():
-    x = 1
-    y = 1
-    turtle.shape('turtle')
-    for j in range(360):
-        for i in range(180):
-            turtle.goto(x,y)
-            turtle.right(x)
-        x = x + 10
+def spiral(n, h = 10 , m = 60, r=10):
+    for i in range(n):
+        for _ in range(m):
+            r +=1
+            a = 2*r*math.sin(math.pi/m)
+            turtle.forward(a)
+            turtle.right(360/m)
 
 if __name__ == "__main__":
-    spiral()
-    while True:
-        pass
+    turtle.shape('turtle')
+    spiral(10)
