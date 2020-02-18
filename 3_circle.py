b8d25f2026658dfaa714e1b7dcc380648e61aca7
KoliadaKol <koliadayaroslav2003@gmail.com>
2020-02-18 19:20:16 +1000

    done

--- 3_circle.py
+++ 3_circle.py
@@ -1,11 +1,11 @@
 import turtle
 
 turtle.speed(0)
-def circle():
+def circle(R, x=0, y=0):
     turtle.shape('turtle')
-    for i in range(360):
-        turtle.forward(3)
-        turtle.right(1)
+    for y in range(-R,R+1):
+         
+
 
 if __name__ == "__main__":
     circle()
