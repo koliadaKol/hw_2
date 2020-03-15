e9eb7559bd583a156c2e5abc1a836beb7a61d1eb
koliadaKol <koliadayaroslav2003@gmail.com>
2020-03-15 11:06:43 +1000

    done

--- 14_pig.py
+++ 14_pig.py
@@ -1,37 +1,42 @@
 import turtle
 
 
-turtle.speed(0.01)
+turtle.speed(0.001)
 def rect_pig(i = 0):
     turtle.shape('turtle')
 
 
-    turtle.penup()
-    turtle.color("black")
-    turtle.goto(10, 70)
-    turtle.pendown()
-    turtle.begin_fill()
-    for i in range(130):
-        turtle.forward(1)
-        turtle.right(3)
-    turtle.color("pink")
-    turtle.end_fill()
-
-    turtle.penup()
-    turtle.color("black")
-    turtle.goto(0, 0)
-    turtle.pendown()
     turtle.begin_fill()
     for i in range(360):
         turtle.forward(3)
         turtle.right(1)
-    turtle.color("pink")
+    turtle.color("red")
+    turtle.end_fill()
+
+    turtle.penup()
+    turtle.color("red")
+    turtle.goto(70, 15)
+    turtle.pendown()
+    turtle.begin_fill()
+    for i in range(180):
+        turtle.forward(2)
+        turtle.right(2)
+    turtle.end_fill()
+
+    turtle.penup()
+    turtle.color("red")
+    turtle.goto(-70, 15)
+    turtle.pendown()
+    turtle.begin_fill()
+    for i in range(180):
+        turtle.forward(2)
+        turtle.right(2)
     turtle.end_fill()
 
 
     turtle.penup()
     turtle.color("black")
-    turtle.goto(-80, -80)
+    turtle.goto(-70, -80)
     turtle.pendown()
     turtle.begin_fill()
     for i in range(180):
@@ -41,7 +46,7 @@ def rect_pig(i = 0):
     turtle.end_fill()
     turtle.penup()
     turtle.color("black")
-    turtle.goto(-60, -100)
+    turtle.goto(-70, -110)
     turtle.pendown()
     turtle.begin_fill()
     for i in range(120):
@@ -61,7 +66,7 @@ def rect_pig(i = 0):
     turtle.end_fill()
     turtle.penup()
     turtle.color("black")
-    turtle.goto(120, -100)
+    turtle.goto(100, -110)
     turtle.pendown()
     turtle.begin_fill()
     for i in range(120):
@@ -71,14 +76,16 @@ def rect_pig(i = 0):
 
     turtle.penup()
     turtle.goto(10, -190)
+    turtle.width(1.5)
     turtle.pendown()
     for i in range(340):
         turtle.forward(1)
         turtle.right(1.1)
 
     turtle.penup()
+    turtle.width(1)
     turtle.color("black")
-    turtle.goto(-20, -220)
+    turtle.goto(-10, -220)
     turtle.pendown()
     turtle.begin_fill()
     for i in range(120):
@@ -88,13 +95,17 @@ def rect_pig(i = 0):
 
     turtle.penup()
     turtle.color("black")
-    turtle.goto(35, -220)
+    turtle.goto(45, -220)
     turtle.pendown()
     turtle.begin_fill()
     for i in range(120):
         turtle.forward(1)
         turtle.right(3)
     turtle.end_fill()
+
+    turtle.penup()
+    turtle.color("white")
+    turtle.goto(300, -300)
 
 if __name__ == "__main__":
     rect_pig()
